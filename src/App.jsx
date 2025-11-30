3df595e723dc3ed21334a462040af50b9b781961
Avishek roy <avishekroy87@gmail.com>
2025-11-30 18:37:16 +0530

    refactor30.11.2025

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,23 @@
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import Timer from "./Timer";
 
 const App = () => {
-
   const [count, setCount] = useState(0);
 
   function increment(times) {
-    setCount(prev => prev + times);
-
+    setCount((prev) => prev + times);
   }
 
   return (
-    <>
+    <Fragment>
       <button onClick={() => increment(1)}>1+</button>
       <button onClick={() => increment(5)}>5+</button>
       <button onClick={() => increment(10)}>10+</button>
       {count}
 
       <Timer />
-    </>
+    </Fragment>
   );
-}
+};
 
 export default App;
--- src/Timer.jsx
+++ src/Timer.jsx
@@ -1,29 +1,26 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 
 function Timer() {
-
-    const [count, setCount] = useState(0)
-    const [isrunning, setIsrunning] = useState(true)
+    const [count, setCount] = useState(0);
+    const [isrunning, setIsrunning] = useState(true);
 
     useEffect(() => {
         if (!isrunning) return;
 
         const interval = setInterval(() => {
-            setCount(prev => prev + 1)
+            setCount((prev) => prev + 1);
         }, 1000);
 
         return () => clearInterval(interval);
-
-
-    }, [isrunning])
+    }, [isrunning]);
     return (
         <>
             <div>Timer</div> {count}
-            <button onClick={() => setIsrunning(!isrunning)}>{isrunning ? "Pause" : "Start"}</button>
+            <button onClick={() => setIsrunning(!isrunning)}>
+                {isrunning ? "Pause" : "Start"}
+            </button>
         </>
-
-
-    )
+    );
 }
 
-export default Timer
+export default Timer;
