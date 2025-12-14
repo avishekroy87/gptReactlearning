ed739dc6570f923774ed3107ac19aa804b8443a2
Avishek roy <avishekroy87@gmail.com>
2025-12-14 21:47:56 +0530

    14Dec25

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
-import { Fragment, useState } from "react";
+import { Fragment} from "react";
 import Timer from "./Timer";
+import TooltipButton from "./Daythird";
+
 
 const App = () => {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
-  function increment(times) {
-    setCount((prev) => prev + times);
-  }
+  // function increment(times) {
+  //   setCount((prev) => prev + times);
+  // }
 
   return (
     <Fragment>
-      <button onClick={() => increment(1)}>1+</button>
+      {/* <button onClick={() => increment(1)}>1+</button>
       <button onClick={() => increment(5)}>5+</button>
       <button onClick={() => increment(10)}>10+</button>
       {count}
 
-      <Timer />
+      <Timer /> */}
+      <TooltipButton />
     </Fragment>
   );
 };
